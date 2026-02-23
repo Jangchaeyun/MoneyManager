70f0a710fd6ee3d1e4f4d7f0a656e183624f88b3
Jangchaeyun <zzangsally@naver.com>
2026-02-23 22:32:06 +0900

    0223 이메일 서비스

---
+++ moneymanager/src/main/java/com/cherry/manager/controller/ProfileController.java
@@ -0,0 +1,35 @@
+package com.cherry.manager.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cherry.manager.dto.ProfileDTO;
+import com.cherry.manager.service.ProfileService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+	private final ProfileService profileService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+		ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+	}
+	
+	@GetMapping("/activate")
+	public ResponseEntity<String> activateProfile(String activationToken) {
+		boolean isActivated = profileService.activateProfile(activationToken);
+		if (isActivated) {
+			return ResponseEntity.ok("Profile activated successfully");
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activation token not found or already used");
+		}
+	}
+}
---
+++ moneymanager/src/main/java/com/cherry/manager/service/ProfileService.java
@@ -0,0 +1,64 @@
+package com.cherry.manager.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.cherry.manager.dto.ProfileDTO;
+import com.cherry.manager.entity.ProfileEntity;
+import com.cherry.manager.repository.ProfileRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+	private final ProfileRepository profileRepository;
+	private final EmailService emailService;
+	
+	public ProfileDTO registerProfile(ProfileDTO profileDTO) {
+		ProfileEntity newProfile = toEntity(profileDTO);
+		
+		newProfile.setActivationToken(UUID.randomUUID().toString());
+		newProfile = profileRepository.save(newProfile);
+		String activateionLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+		String subject = "Money Manager 계정을 활성화하세요";
+		String body = "다음 링크를 클릭하여 계정을 활성화하세요: " + activateionLink;
+		emailService.sendEmail(newProfile.getEmail(), subject, body);
+		
+		return toDTO(newProfile);
+	}
+	
+	public ProfileEntity toEntity(ProfileDTO profileDTO) {
+		return ProfileEntity.builder()
+				.id(profileDTO.getId())
+				.fullName(profileDTO.getFullName())
+				.email(profileDTO.getEmail())
+				.password(profileDTO.getPassword())
+				.profileImageUrl(profileDTO.getProfileImageUrl())
+				.createdAt(profileDTO.getCreatedAt())
+				.updatedAt(profileDTO.getUpdatedAt())
+				.build();
+	}
+	
+	public ProfileDTO toDTO(ProfileEntity profileEntity) {
+		return ProfileDTO.builder()
+				.id(profileEntity.getId())
+				.fullName(profileEntity.getFullName())
+				.email(profileEntity.getEmail())
+				.profileImageUrl(profileEntity.getProfileImageUrl())
+				.createdAt(profileEntity.getCreatedAt())
+				.updatedAt(profileEntity.getUpdatedAt())
+				.build();
+	}
+	
+	public boolean activateProfile(String activationToken) {
+		return profileRepository.findByActivationToken(activationToken)
+				.map(profile -> {
+					profile.setIsActive(true);
+					profileRepository.save(profile);
+					return true;
+				})
+				.orElse(false);
+	}
+}
