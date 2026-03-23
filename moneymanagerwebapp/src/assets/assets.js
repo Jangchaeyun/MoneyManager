d6d77b3c53d5e764a2b4b821d4cdc833ed63a3fb
Jangchaeyun <zzangsally@naver.com>
2026-03-23 21:59:24 +0900

    0323 feat: implement sidebar component and enhance dashboard with user context integration

--- moneymanagerwebapp/src/assets/assets.js
+++ moneymanagerwebapp/src/assets/assets.js
@@ -5,3 +5,36 @@ export const assets = {
   logo,
   login_bg,
 };
+
+export const SIDE_BAR_DATA = [
+  {
+    id: "01",
+    label: "대시보드",
+    icon: LayoutDashboard,
+    path: "/dashboard",
+  },
+  {
+    id: "02",
+    label: "카테고리",
+    icon: List,
+    path: "/category",
+  },
+  {
+    id: "03",
+    label: "소득",
+    icon: Wallet,
+    path: "/income",
+  },
+  {
+    id: "04",
+    label: "비용",
+    icon: Coins,
+    path: "/expense",
+  },
+  {
+    id: "05",
+    label: "필터",
+    icon: FunnelPlus,
+    path: "/filter",
+  },
+];
--- moneymanagerwebapp/src/components/Dashboard.jsx
+++ moneymanagerwebapp/src/components/Dashboard.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Menubar from "./Menubar";
+import { AppContext } from "../context/AppContext";
+import Sidebar from "./Sidebar";
 
 const Dashboard = () => {
+  const { user } = useContext(AppContext);
   return (
     <div>
       <Menubar />
+
+      {user && (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            <Sidebar />
+          </div>
+          <div className="grow mx-5">Ride side</div>
+        </div>
+      )}
     </div>
   );
 };
