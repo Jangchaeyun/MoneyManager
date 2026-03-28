a27209f5146f00d4e2d5a9389015a53c63df45c6
Jangchaeyun <zzangsally@naver.com>
2026-03-28 22:02:55 +0900

    0328 feat: enhance AddCategoryForm with Input component and add EmojiPickerPopup

--- moneymanagerwebapp/src/components/AddCategoryForm.jsx
+++ moneymanagerwebapp/src/components/AddCategoryForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Input from "./input";
 
 const AddCategoryForm = () => {
   const [category, setCategory] = useState({
@@ -10,11 +11,26 @@ const AddCategoryForm = () => {
     { value: "income", label: "소득" },
     { value: "expense", label: "비용" },
   ];
+
+  const handleChange = (key, value) => {
+    setCategory({ ...category, [key]: value });
+  };
   return (
     <div className="p-4">
       <Input
         value={category.name}
         onChange={({ target }) => handleChange("name", target.value)}
+        label="카테고리 이름"
+        placeholder="예: 프리랜서, 급여, 식료품"
+        type="text"
+      />
+
+      <Input
+        label="카테고리 타입"
+        value={category.type}
+        onChange={({ target }) => handleChange("type", target.value)}
+        isSelect={true}
+        options={categoryTypeOptions}
       />
     </div>
   );
